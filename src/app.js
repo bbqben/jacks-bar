20cf6183f0595ba26c305cfd17da026560d15dbc
Ben Tran <bentran@Bens-MacBook-Pro.local>
2018-05-05 17:10:18 -0400

    Update: updated app.js as a simple component which renders text

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-console.log("It Works!");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Hello World!</p>
+            </div>
+        )
+    } 
+} // End of class App
+
+ReactDOM.render(<App />, document.getElementById('app'));

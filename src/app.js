fd722ea66789b5d79c88e28dad24bad653b8ce54
Ben Tran <bentran@Bens-MacBook-Pro.local>
2018-05-06 19:07:31 -0400

    Update: Included firebase and updated app.js to interact with db

--- src/app.js
+++ src/app.js
@@ -1,20 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//Imported Helpers
+import firebase from 'firebase';
+import firebaseConfig from "./components/helper/firebaseConfig";
 
 class App extends React.Component {
     constructor() {
         super();
         this.state = {
+            items: [],
         }
     }
+
+    componentDidMount() {
+        firebase.initializeApp(firebaseConfig);
+
+        this.getFBItem(
+            "items",
+            (items) => {
+                // console.log(items)
+                let newState = [];
+                for (let item in items) {
+                    newState.push({
+                        id: item,
+                        title: items[item].title,
+                    });
+                }
+                console.log(newState);
+                this.setState({items: newState})
+            }
+        )
+    }
+
+
 
     render() {
         return (
             <div>
-                <p>Hello World!</p>
+                <h1>List of Notes:</h1>
+                <ul>
+                    {
+                    this.state.items.map((item, index) => {
+                        return (
+                            <li key={item.id}>
+                                <p>{item.title}</p>
+                            </li>
+                        )
+                    })
+                }
+                </ul>
             </div>
         )
     } 
-} // End of class App
+
+    getFBItem(item, callBack) {
+        const itemsRef = firebase.database().ref(item)
+        itemsRef.on('value', (snapshot) => {
+            callBack(snapshot.val());
+        });
+    }
+} 
 
 ReactDOM.render(<App />, document.getElementById('app'));
---
+++ src/components/helper/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA3cFqvbXjJ0jsd9vP4rKk-6WEy_Cik6Dk",
+  authDomain: "react-firebase-playgroun-d0a33.firebaseapp.com",
+  databaseURL: "https://react-firebase-playgroun-d0a33.firebaseio.com",
+  projectId: "react-firebase-playgroun-d0a33",
+  storageBucket: "react-firebase-playgroun-d0a33.appspot.com",
+  messagingSenderId: "668111232257"
+};
+export default firebaseConfig;
